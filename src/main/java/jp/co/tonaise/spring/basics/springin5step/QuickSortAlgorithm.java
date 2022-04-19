dced871668025c0baaf9606e9f8dadd98c58c88c
tonaise <object1985@gmail.com>
2022-04-20 00:15:06 +0900

    [add]try annotation @Component @Primary

--- src/main/java/jp/co/tonaise/spring/basics/springin5step/QuickSortAlgorithm.java
+++ src/main/java/jp/co/tonaise/spring/basics/springin5step/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package jp.co.tonaise.spring.basics.springin5step;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
     @Override
     public int[] sort(int[] numers) {
